94afe314c6ae690238332f83bfefa9b86b43e7ac
adaken4 <adakennedy@outlook.com>
2025-01-24 20:54:36 +0300

    [feat]: add createBoard function that creates the board and adds the game elements like dots, wall, etc.

--- app.js
+++ app.js
@@ -41,11 +41,28 @@ document.addEventListener('DOMContentLoaded', function() {
     // 3 - power-pellet
     // 4 - empty
 
+    const squares = [];
+
     // create board
     function createBoard() {
         for (let i = 0; i < layout.length; i++) {
             const square = document.createElement('div');
             grid.appendChild(square);
+            squares.push(square);
+
+            // add layout to the board
+            if (layout[i] === 0) {
+                squares[i].classList.add('pac-dot');
+            }
+            if (layout[i] === 1) {
+                squares[i].classList.add('wall');
+            }
+            if (layout[i] === 2) {
+                squares[i].classList.add('ghost-lair');
+            }
+            if (layout[i] === 3) {
+                squares[i].classList.add('power-pellet');
+            }
         }
     }
 
